d7acc10fd449f7552b3cbac64df14897b9a69482
Michael Smirnoff <michaelsmirnoff@live.com>
2016-04-19 11:12:28 +0930

    Use the LogEntries built-in tag support

--- lib/production_toolkit/initializers/logging.rb
+++ lib/production_toolkit/initializers/logging.rb
@@ -2,7 +2,6 @@ if defined?(Rails)
   if Rails.env.production?
     require 'le'
     log_entries_key = Rails.application.secrets.log_entries_key
-    le = Le.new(log_entries_key, ssl: true)
-    Rails.logger = ActiveSupport::TaggedLogging.new(le)
+    Rails.logger = Le.new(log_entries_key, ssl: true, tag: true)
   end
 end
