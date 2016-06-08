cfdbb7ed8fe4640899b71d65b9b87e606ff3c1c9
Michael Noack <michael@noack.com.au>
2016-06-08 20:47:50 +0930

    Fix log setting, must set constant, not symbol

--- lib/production_toolkit/initializers/logging.rb
+++ lib/production_toolkit/initializers/logging.rb
@@ -2,7 +2,8 @@ if defined?(Rails)
   if Rails.env.production?
     require 'le'
     log_entries_key = Rails.application.secrets.log_entries_key
-    log_level = Rails.application.config.log_level || :info
+    log_level_symbol = Rails.application.config.log_level || :info
+    log_level = Logger.const_get(log_level_symbol.to_s.upcase)
     Rails.logger = Le.new(log_entries_key, ssl: true, tag: true, log_level: log_level)
   end
 end
